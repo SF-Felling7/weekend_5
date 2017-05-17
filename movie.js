1ac20ab0a0e21fadb39f093ba6acfd572b281621
Sean Felling <Snfelling7@gmail.com>
2017-05-17 08:33:41 -0500

    final update

--- movie.js
+++ movie.js
@@ -1,51 +1,65 @@
 //REQUIRES
-var express = require( 'express' );
+var express = require('express');
 var app = express();
-var path = require( 'path' );
-var bodyParser = require( 'body-parser' );
-var mongoose = require( 'mongoose' );
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 
 var port = process.env.PORT || 4379;
 
 //USES
-app.use(express.static( 'public' ) );
-app.use(bodyParser.urlencoded( {extended:true}));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 
 //CREATE MONGODB
 
-mongoose.connect( 'localhost:27017/movieCollection' );
+mongoose.connect('localhost:27017/movieCollection');
 
 //CREATE MONGOOSE SCHEMA
 
 var movieSchema = mongoose.Schema({
-  title: String,
-  year: Number,
-  poster:String
+    title: String,
+    year: Number,
+    poster: String
 });
 
-var movies = mongoose.model( 'movies', movieSchema );
+var movies = mongoose.model('movies', movieSchema);
 
 
 //INITIAL GET, SERVER INDEX FILE
-app.get( '/', function( req, res ) {
-  console.log( 'hit base url!');
-  res.sendFile( path.resolve( './public/views/index.html' ) );
+app.get('/', function(req, res) {
+    console.log('hit base url!');
+    res.sendFile(path.resolve('./public/views/index.html'));
 });
 
-app.post( '/postFav', function( req, res ) {
-  console.log('hit post');
-  console.log('req.body =', req.body);
-  var newMovie = movies( req.body) ;
-  newMovie.save().then(function() {
-    res.sendStatus( 200 );
+app.post('/postFav', function(req, res) {
+    console.log('hit post');
+    console.log('req.body =', req.body);
+    var newMovie = movies(req.body);
+    newMovie.save().then(function() {
+        res.sendStatus(200);
+    });
+
+});
+
+app.get( '/appendFav', function( req, res ) {
+console.log( 'hit  appendFave route' );
+  movies.find({}, function(err, results) {
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }else{
+      console.log('successful get heros ->', results);
+      res.status(200).send(results);
+    }
   });
-
 });
 
 
-
-app.listen( port, function() {
-  console.log( 'listening on port:', port );
+app.listen(port, function() {
+    console.log('listening on port:', port);
 });
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,31 +7,35 @@ MovieApp.controller('MovieAppController', function($http) {
 
     var vm = this;
     vm.movies = [];
+    vm.favorites = [];
+
 
     vm.getMovies = function() {
         console.log('getting movies');
-        return $http({
+         $http({
             method: 'GET',
             url: 'http://www.omdbapi.com/?s=' + vm.title
         }).then(function(response) {
             console.log('response', response.data);
             vm.movies = response.data.Search;
         });
+        vm.title = " ";
+        vm.favorites = [];
 
     };
 
-vm.favorites = [];
 
-vm.postFav = function (title, year, poster) {
-console.log('In post faves');
-var objectToSend = {
-Title: title,
-Year: year,
-Poster: poster
-// ImdbID: ms.imdbID
-};
+
+    vm.postFav = function(title, year, poster) {
+        console.log('In post faves');
+        var objectToSend = {
+            title: title,
+            year: year,
+            poster: poster
+            // ImdbID: ms.imdbID
+        };
         console.log('objectToSend ->', objectToSend);
-        return $http({
+         $http({
             method: 'POST',
             url: '/postFav',
             data: objectToSend
@@ -39,6 +43,15 @@ Poster: poster
             console.log('response');
         });
 
+    };
+
+    vm.getFaves = function() {
+      $http ({
+        method:'GET',
+        url:'/appendFav'
+      }).then(function success(response) {
+        vm.favorites = response.data;
+      });
     };
 
 });
